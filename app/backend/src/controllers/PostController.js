d42ac4ac665926539d0c692bae893039d9e1387c
Smeet <smeetagrawal2003@gmail.com>
2023-11-15 00:59:40 +0530

    ➕Delete Post and ➕user and post model connection

--- app/backend/src/controllers/PostController.js
+++ app/backend/src/controllers/PostController.js
@@ -1,35 +1,43 @@
+const User = require("../model/UserModel");
 const Post = require("../model/PostModel");
 const asyncHandler = require("express-async-handler");
 const path = require('path');
 
 
-const PostUpload = async(req,res)=>{
-    const {title,content} = req.body;
+const PostUpload = async (req, res) => {
+    const { title, content } = req.body;
     const user_id = req.user.id;
     console.log(user_id);
-    if(!title || !content)
-    {
+    if (!title || !content) {
         res.status(400);
         throw new Error("All Fields are mandotory");
     }
-    const imagePath = req.file.path;
+    // const imagePath = req.file.path;
 
-    try{
+    try {
         const post = await Post.create(
             {
-                userid:user_id,
-                title:title,
-                content:content,
-                imagePath: imagePath,
-               
+                userid: user_id,
+                title: title,
+                content: content,
+                imagePath: "abc",
+
             }
         )
         console.log(post);
-        
-        if (post) {
-            const savedPost = await post.save();
 
-        res.status(201).json(savedPost);
+        if (post) {
+            post.save().then((post) => {
+                User.findOneAndUpdate({ _id: user_id }, { $push: { posts: post._id } })
+                    .then(() => {
+                        console.log('Post added to user successfully');
+                    })
+                    .catch((err) => {
+                        console.error('Error updating user:', err);
+                    });
+            });
+
+            res.status(201);
         }
         else {
             res.status(400);
@@ -43,12 +51,11 @@ const PostUpload = async(req,res)=>{
 
 
 }
-const GetPost =  async (req, res) => {
+const GetPost = async (req, res) => {
     try {
         const userId = req.params.userId;
         const userPosts = await Post.find({ userid: userId });
 
-        // Render a view (you need to have a view engine set up, like EJS, Handlebars, or Pug)
         res.render('userPosts', { posts: userPosts });
     } catch (error) {
         console.error(error);
@@ -56,7 +63,33 @@ const GetPost =  async (req, res) => {
     }
 }
 
+
+const DeletePost = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const postId = req.params.postid;
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        user.posts.pull(postId);
+        await user.save();
+
+        await Post.deleteOne({ _id: postId });
+        await Post.save();
+
+        res.status(200).json({ message: 'Post deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+
 module.exports = {
     PostUpload,
-    GetPost
+    GetPost,
+    DeletePost
 }
--- app/backend/src/routes/PostRoutes.js
+++ app/backend/src/routes/PostRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const{PostUpload,GetPost} = require("../controllers/PostController");
+const{
+    PostUpload,
+    GetPost,
+    DeletePost
+} = require("../controllers/PostController");
 const validateToken = require("../middleware/validateToken");
 const upload = require("../../utils/multer")
 
 
-router.post("/",validateToken,upload.single('file'),PostUpload);
+router.post("/CreatePost",validateToken,upload.single('file'),PostUpload);
+router.get("/getPost",validateToken,GetPost);
+router.delete("/deletePost",validateToken,DeletePost);
 
 
 
