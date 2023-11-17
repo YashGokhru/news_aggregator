d18c2f767ba8a47cfef4a46cb615e2479f9f1cae
siddhant <sidpatel1436@gmail.com>
2023-11-18 01:04:16 +0530

    News_Aggregator: Frontend: Backend: Modify Login_page

--- app/backend/src/index.js
+++ app/backend/src/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const cors = require('cors');
+
+// require('ejs');
 require("../src/config/DbConnection");
 const app = express();
 const dotenv = require("dotenv").config();
@@ -6,9 +9,12 @@ const UserRoutes = require("./routes/UserRoutes");
 const PostRoutes = require("./routes/PostRoutes");
 const cookieParser = require("cookie-parser");
 
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
+app.use(cors());
+// app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res) => {
@@ -21,6 +27,6 @@ app.use("/post",PostRoutes);
 
 
 
-app.listen(process.env.PORT, () => {
+app.listen(5175, () => {
   console.log("Server listening on port " + process.env.PORT + "✅");
 });
