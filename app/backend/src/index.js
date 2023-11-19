48d25692573dba23b637b11f5da3f3f5baada588
YashGokhru <yashgokhru28@gmail.com>
2023-11-20 03:21:41 +0530

    Home Page

--- app/backend/src/index.js
+++ app/backend/src/index.js
@@ -7,7 +7,7 @@ const UserRoutes = require("./routes/UserRoutes");
 const PostRoutes = require("./routes/PostRoutes");
 const HomeRoutes = require("./routes/HomeRoutes");
 const SearchRoutes = require("./routes/SearchRoutes");
-const HomeRoutes = require("./routes/HomeRoutes");
+// const HomeRoutes = require("./routes/HomeRoutes");
 const cookieParser = require("cookie-parser");
 const path = require('path');
 
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get("/", (req, res) => {
-  res.send("Login");
+  res.render("login");
 });
 
 app.get("/register", (req, res) => {
@@ -38,7 +38,7 @@ app.get("/home", (req, res) => {
 const port = process.env.PORT || 3000
 app.use("/user",UserRoutes);
 app.use("/post",PostRoutes);
-app.use("/home",HomeRoutes);
+app.use("/posts",HomeRoutes);
 app.use("/search",SearchRoutes);
 //process.env.PORT
 app.listen(port, () => {
