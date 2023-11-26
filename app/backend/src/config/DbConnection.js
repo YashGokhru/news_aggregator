a034021c6bedba2748acbed9b9373c5af230bafc
Smeet <smeetagrawal2003@gmail.com>
2023-11-27 01:00:32 +0530

    ➕Logout Script

--- app/backend/src/config/DbConnection.js
+++ app/backend/src/config/DbConnection.js
@@ -1,22 +1,9 @@
 const mongoose = require('mongoose');
 
-const connectDb = async () => {
-  try {
-    await mongoose.connect('mongodb+srv://G21:2iyH5zsC8Nt$3UQ@cluster0.ybfv1xy.mongodb.net/NewsAggregator', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Database connected');
-  } catch (error) {
-    console.error(error);
-  }
-};
 
-const disconnectDb = async () => {
-  await mongoose.disconnect();
-};
+const connectDb = mongoose.connect('mongodb+srv://G21:2iyH5zsC8Nt$3UQ@cluster0.ybfv1xy.mongodb.net/NewsAggregator')
+.then(()=>console.log("Database connected")) // if your database has auth 
+.catch((err)=>console.log(err));
 
 
-
-// Exporting the connection functions in case they are needed elsewhere
-module.exports = { connectDb, disconnectDb };
+module.exports = connectDb;
