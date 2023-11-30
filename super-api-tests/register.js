e10350e36454cffec9191a593e1106b42dfc08d7
RohitThakre980 <143123084+RohitThakre980@users.noreply.github.com>
2023-11-30 14:15:30 +0530

    Update and rename super-api-tests/test/user_testing.js to super-api-tests/register.js
    change in user_testing

--- super-api-tests/register.js
+++ super-api-tests/register.js
@@ -1,48 +1,48 @@
 import supertest from 'supertest';
-const request = supertest('http://localhost:3000');
-const TOKEN = process.env.USER_TOKEN; 
+const request = supertest('https://news-aggregator-lsjs.onrender.com/');
+const TOKEN =  process.env.ACCESS_TOKEN_SECRET; 
 const { expect } = require('chai');
 
 export const createRandomUser = async () => {
     const userdata = {
 
  email: `test${Math.floor(Math.random() * 9999)}@mail.ca`,
-name: 'testes',
-password:'1234'
+ name: 'testes',
+ password:'1234'
     };
 
    const res = await request
-    .post('users')
-    .set('Authorization',`Bearer ${TOKEN}`)
+    .post('createpost')
+    .set('Authorization', `Bearer ${TOKEN}`)
     .send(userdata)
 
     return res.body.id
         
 };
 
+let postId;  
 
+describe('User post validation', () => {
 
-let postId; 
-
-describe('User validation', () => {
-
-    it('/users', async () => {
+    it('/register', async () => {
         
         const data = {
            
-          email: `test${Math.floor(Math.random() * 9999)}@mail.ca`,
-          name: 'testes',
-          password:'1234'
+            email: `test${Math.floor(Math.random() * 9999)}@mail.ca`,
+            name: 'testes',
+            password:'1234',
+            posts:postId
+      
             
         };
 
         try {
             const postres = await request
-                .post('users')
+                .post('register')
                 .set('Authorization', `Bearer ${TOKEN}`)
                 .send(data);
 
-            // console.log('Response:', postres.body); 
+            console.log('Response:', postres.body); 
              console.log(data);
            
             expect(postres.status).to.satisfy((status) => {
@@ -54,7 +54,7 @@ describe('User validation', () => {
             if (postres.body.data) {
                 postId = postres.body.data.id;
             }
-            // console.log(postres.body);
+             console.log(postres.body);
            
         } catch (error) {
             console.error('Error:', error); 
@@ -71,11 +71,11 @@ describe('User validation', () => {
     
 
    
-    it('GET /users/:id', async () => {
+    it('GET /register/:id', async () => {
         
                 try {
                     const response = await request
-                        .get(`users/${postId}`)
+                        .get(`register/${postId}`)
                         .set('Authorization', `Bearer ${TOKEN}`)
                         .expect(404);
             
@@ -92,15 +92,16 @@ describe('User validation', () => {
             });
             
     describe('Positive Tests', () => {
-        it.only('201 Every Things Is Okk', async () => {
+        it('201 Every Things Is Okk', async () => {
             const data = {
             email: `test${Math.floor(Math.random() * 9999)}@mail.ca`,
             name: 'testes',
-            password:'1234'
+            password:'1234',
+            posts:postId
             };
     
             const postRes = await request
-                .post('users')
+                .post('register')
                 .set('Authorization', `Bearer ${TOKEN}`)
                 .send(data);
     
@@ -117,16 +118,17 @@ describe('User validation', () => {
     });
     describe('Negative  Tests', () => {
         
-        it('422 Validation Failed', async () => {
+        it('422 Validation Failed Name Is Empty', async () => {
             const data = 
             {
                 email: `test${Math.floor(Math.random() * 9999)}@mail.ca`,
                 name: '',
-                password:'1234'
+                password:'1234',
+                posts:postId
             };
         
             const postRes = await request
-                .post('users')
+                .post('register')
                 .set('Authorization', `Bearer ${TOKEN}`)
                 .send(data);
         
@@ -144,11 +146,11 @@ describe('User validation', () => {
                 expect(postRes.body.message).to.be.undefined;
                 
         });
-        it('should return 404 for non-existing user', async () => {
+        it('should return 404 for token not used', async () => {
             try {
                 const getUserResponse = await request
-                    .get(`users/${postId}`)
-                    .set('Authorization', `Bearer ${TOKEN}`)
+                    .get(`register/${postId}`)
+                    // .set('Authorization', `Bearer ${TOKEN}`)
                     .expect(404);
     
                 console.log('Get User Response:', getUserResponse.body);
@@ -162,6 +164,34 @@ describe('User validation', () => {
                 console.error('Error getting user:', error);
                 throw error;
             }
+        });
+        it('422 Validation Failed Name Is Empty', async () => {
+            const data = 
+            {
+                email: `test${Math.floor(Math.random() * 9999)}@mail.ca`,
+                name: 'test12',
+                password:'',
+                posts:postId,
+            };
+        
+            const postRes = await request
+                .post('register')
+                .set('Authorization', `Bearer ${TOKEN}`)
+                .send(data);
+        
+                console.log(postRes.status);
+                console.log(postRes.body);
+                expect(postRes.status).to.eq(422);
+                expect(postRes.body).to.exist; 
+                
+                if (postRes.body.field !== undefined) {
+                    expect(postRes.body.field).to.be.an('array').that.includes('password');
+                } else {
+                    
+                }
+                
+                expect(postRes.body.message).to.be.undefined;
+                
         });
    
 });
